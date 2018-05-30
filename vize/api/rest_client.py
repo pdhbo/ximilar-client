0aef954dd25f1001363fab7b43f125b41ed455d0
MichalLukac <cospelthetraceur@gmail.com>
2018-05-30 11:43:09 +0200

    Update getting images from vize.ai

--- vize/api/rest_client.py
+++ vize/api/rest_client.py
@@ -20,15 +20,38 @@ class RestClient(object):
                         'Authorization': 'Token ' + self.api_key}
 
     def get(self, api_endpoint, data=None):
+        """
+        Sends the http get request
+        :param api_endpoint: url endpoint
+        :param data: json_data to send
+        :return: json/dict result
+        """
         return requests.get(self.endpoint+api_endpoint, headers=self.headers, data=data).json()
 
     def post(self, api_endpoint, data=None, files=None):
+        """
+        Sends the http post request
+        :param api_endpoint: url endpoint
+        :param data: json_data to send
+        :return: json/dict result
+        """
         return requests.post(self.endpoint+api_endpoint, headers=self.headers, data=json.dumps(data), files=files).json()
 
     def delete(self, api_endpoint, data=None):
+        """
+        Sends the http delete request
+        :param api_endpoint: url endpoint
+        :param data: json_data to send
+        :return: json/dict result
+        """
         return requests.delete(self.endpoint+api_endpoint, headers=self.headers, data=data).json()
 
     def load_base64_file(self, path):
+        """
+        Load file/image to base64.
+        :param path: local path to the image
+        :return: base64 encoded string
+        """
         with open(path, 'rb') as file_img:
             image = base64.b64encode(file_img.read()).decode('utf-8')
         return image
@@ -39,6 +62,10 @@ class VizeRestClient(RestClient):
         return Task(self.api_key, self.endpoint, self.get(TASK_ENDPOINT + id_task))
 
     def get_all_tasks(self):
+        """
+        Get all tasks of the user(user is specified by api key).
+        :return: List of tasks
+        """
         result = self.get(TASK_ENDPOINT)
         return [Task(self.api_key, self.endpoint, task_json) for task_json in result[RESULTS]]
 
@@ -52,6 +79,10 @@ class VizeRestClient(RestClient):
         return self.post(LABEL_ENDPOINT, data={NAME: name})
 
     def get_all_labels(self):
+        """
+        Get all labels of the user(user is specified by api key).
+        :return: List of labels
+        """
         result = self.get(LABEL_ENDPOINT)
         return [Label(self.api_key, self.endpoint, label_json) for label_json in result[RESULTS]]
 
@@ -59,13 +90,14 @@ class VizeRestClient(RestClient):
         return Label(self.api_key, self.endpoint, self.get(LABEL_ENDPOINT+id_label))
 
     def upload_image(self, file_path, labels):
+        # TODO this method
         raise NotImplementedError
 
     def get_image(self, id_image):
-        raise NotImplementedError
+        return Image(self.api_key, self.endpoint, self.get(IMAGE_ENDPOINT+id_image))
 
     def remove_image(self, id_image):
-        raise NotImplementedError
+        return self.delete(IMAGE_ENDPOINT+id_image)
 
 
 class Task(VizeRestClient):
@@ -106,10 +138,14 @@ class Task(VizeRestClient):
         return self.post(CLASSIFY_ENDPOINT, data=data)
 
     def add_label(self, id_label):
-        raise NotImplementedError
+        if not self.frozen:
+            return self.post(TASK_ENDPOINT+self.id+'/add-label/', data={'label_id': id_label})
+        return {'status': 'error', 'message': 'cannot add label to this task, task is frozen'}
 
     def remove_label(self, id_label):
-        raise NotImplementedError
+        if not self.frozen:
+            return self.post(TASK_ENDPOINT+self.id+'/remove-label/', data={'label_id': id_label})
+        return {'status': 'error', 'message': 'cannot add label to this task, task is frozen'}
 
 
 class Label(VizeRestClient):
@@ -119,8 +155,17 @@ class Label(VizeRestClient):
         self.id = label_json[ID]
         self.name = label_json[NAME]
 
-    def add_image(self, id_image):
-        raise NotImplementedError
+    def get_training_images(self, page_url=None):
+        """
+        Get paginated result of images for specific label.
+
+        :param page_url: optional, select the specific page of images, default first page
+        :return: (list of images, next_page)
+        """
+        url = page_url if page_url else IMAGE_ENDPOINT
+        result = self.get(url)
+        print(result)
+        return [Image(self.api_key, self.endpoint, image_json) for image_json in result[RESULTS]], result['next']
 
 
 class Image(VizeRestClient):
@@ -128,27 +173,22 @@ class Image(VizeRestClient):
         super(Image, self).__init__(api_key, endpoint)
 
         self.id = image_json[ID]
+        self.thumb_img_path = image_json['thumb_img_path']
+
+    def get_labels(self):
+        return [Label(self.api_key, self.endpoint, label) for label in self.get(IMAGE_ENDPOINT+self.id)['labels']]
 
     def add_label(self, id_label):
-        raise NotImplementedError
+        # todo this works but raises error
+        return self.post(IMAGE_ENDPOINT + self.id + '/add-label/', data={'label_id': id_label})
 
     def remove_label(self, id_label):
-        raise NotImplementedError
+        # todo this works but raises error
+        return self.post(IMAGE_ENDPOINT + self.id + '/remove-label/', data={'label_id': id_label})
 
 
 if __name__ == '__main__':
     client = VizeRestClient('')
-    #test = client.create_label('test label', '')
-    #print(test)
-    #tasks = client.get_all_tasks()
-    #print(tasks)
     task = client.get_task('')
-    #print(task.__dict__)
-    #labels = client.get_all_labels()
-    #print(labels)
-    #label = client.get_label('')
-    #print(label)
-    #task_labels = task.get_labels()
-    #print(task_labels)
+    task_labels = task.get_labels()
     result = task.classify([{'_url': 'http://www.ximilar.com/examples/dubai.jpg'}, {'_file': 'normal.jpg'}])
-    print(result)
