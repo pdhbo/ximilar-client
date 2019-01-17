a5bebc44315caec84d12ae014dc9549b120ac59b
MichalLukac <cospelthetraceur@gmail.com>
2019-01-17 15:11:09 +0100

    Upgrade version and requirements v(1.0.2)

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ from ximilar import __version__
 with open('requirements.txt') as f:
     install_requirements = f.read().splitlines()
 
-setup(name='ximilar-ximilar-client',
+setup(name='ximilar-client',
     version=__version__,
-    description='The Ximilar VIZE.AI API.',
+    description='The Ximilar App and Vize.ai Client.',
     url='http://ximilar.com/',
-    author='Michal Lukac, David Novak',
-    author_email='michallukac@outlook.com',
+    author='Michal Lukac, David Novak and Ximilar.com Team',
+    author_email='tech@ximilar.com',
     license='Apache 2.0',
     packages=find_packages(),
     keywords='machine learning, multimedia, json, rest, data',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.4',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Private :: Do Not Upload'
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
     install_requires=install_requirements,
     include_package_data=True,
--- ximilar/__init__.py
+++ ximilar/__init__.py
@@ -2,4 +2,4 @@ from pkg_resources import declare_namespace
 
 declare_namespace("ximilar")
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
