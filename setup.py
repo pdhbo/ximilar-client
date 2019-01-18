6e17780b6d1c16ffbad84d6812ea1329b17784a4
MichalLukac <cospelthetraceur@gmail.com>
2019-01-18 12:42:10 +0100

    Update versioning of setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 if os.environ.get('CI_COMMIT_TAG'):
     version = os.environ['CI_COMMIT_TAG']
 else:
-    version = os.environ['CI_JOB_ID']
+    version = '2.0.dev1'
 
 
 setup(name='ximilar-client',
