6a130e33c4d0600375c870e123960ce07928b46b
MichalLukac <cospelthetraceur@gmail.com>
2019-04-09 11:05:25 +0200

    Update detection

--- ximilar/client/detection.py
+++ ximilar/client/detection.py
@@ -2,6 +2,7 @@ from ximilar.client import RecognitionClient
 from ximilar.client.constants import *
 
 OBJECT_ENDPOINT = 'detection/v2/object/'
+LABEL_ENDPOINT = 'detection/v2/label/'
 
 
 class DetectionClient(RecognitionClient):
@@ -23,6 +24,38 @@ class DetectionClient(RecognitionClient):
     def get_objects_of_image(self, image_id):
         result = self.get(OBJECT_ENDPOINT + "?image="+image_id)
         return [DetectionObject(self.token, self.endpoint, object_json) for object_json in result[RESULTS]], result['next'], RESULT_OK
+
+
+    def get_all_labels(self):
+        """
+        Get all labels of the user(user is specified by client key).
+        :return: List of labels
+        """
+        url, labels = LABEL_ENDPOINT, []
+
+        while True:
+            result = self.get(url)
+
+            for label_json in result[RESULTS]:
+                labels.append(DetectionLabel(self.token, self.endpoint, label_json))
+
+            if result['next'] is None:
+                break
+
+            url = result['next'].replace(self.endpoint, "").replace(self.endpoint.replace("https", "http"), "")
+
+        return labels, RESULT_OK
+
+
+class DetectionLabel(DetectionClient):
+    def __init__(self, token, endpoint, label_json):
+        super(DetectionLabel, self).__init__(token, endpoint)
+
+        self.id = label_json[ID]
+        self.name = label_json['name']
+
+    def __str__(self):
+        return self.name
 
 
 class DetectionObject(DetectionClient):
