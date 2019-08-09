dba162aabc6f2458956b7c209501edcea8a11d1d
MichalLukac <cospelthetraceur@gmail.com>
2019-08-09 16:11:07 +0200

    Update train workspace

--- tools/train_workspace.py
+++ tools/train_workspace.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     parser.add_argument("--api_prefix", type=str, help="API prefix", default="https://api.ximilar.com/")
     parser.add_argument("--auth_token", help="user authorization token to be used for API authentication")
     parser.add_argument("--workspace_id", help="ID of workspace to upload the images into", default=DEFAULT_WORKSPACE)
+    parser.add_argument("--train_all", help="Train all tasks (even if they are trained)", default=False, action='store_true')
     args = parser.parse_args()
 
     client = RecognitionClient(token=args.auth_token, endpoint=args.api_prefix, workspace=args.workspace_id)
     tasks, status = client.get_all_tasks()
 
     for task in tasks:
-        if not (task.last_train_status == "TRAINED" or task.last_train_status == "IN_TRAINING"):
+        if args.train_all or not (task.last_train_status == "TRAINED" or task.last_train_status == "IN_TRAINING"):
             result = task.train()
-            print("NOT TRAINED", task.name, task.id, result)
+            print("TRAINING", task.name, task.id, result)
