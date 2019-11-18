e14df2686503d8a471c6785239fba88eb5040d8a
David Novak <novak.david@gmail.com>
2019-11-18 15:25:01 +0100

    json

---
+++ tools/json_data.py
@@ -0,0 +1,126 @@
+import gzip
+import json
+import os
+import string
+import sys
+from os.path import splitext
+
+from ximilar.api.python_api import Record
+
+
+def read_json_array(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        array = json.load(f)
+        for rec in array:
+            yield rec
+
+
+def read_json_file_iterator(file_name, required_fields=None, skip=0):
+    """
+    This iterator method reads a file with JSON formatted lines, checks that every line has given field(s) and
+    returns an iterator over JSON objects constructed from these lines.
+    :param file_name: file with JSON formatted lines
+    :param required_fields: list of fields that should be in each JSON object on the lines; lines not matching are not
+            returned
+    :return: an iterator over JSON objects that have given required fields
+    """
+    if splitext(file_name)[1] == ".gz":
+        f = gzip.open(file_name, "rt", encoding="utf-8")
+    else:
+        f = open(file_name, "r", encoding="utf-8")
+
+    chars_to_strip = string.whitespace + ","
+    for line in f:
+        line = line.rstrip(chars_to_strip)
+        if skip > 0:
+            skip -= 1
+            continue
+        try:
+            record = json.loads(line)
+        except UnicodeDecodeError as e:
+            print("wrong encoding: '" + line + "'")
+            continue
+        except ValueError as e:
+            print("JSON error: '" + line + "'")
+            print(e)
+            continue
+
+        if not required_fields or all(field in record for field in required_fields):
+            yield record
+    f.close()
+
+
+def read_json_file_list(file_name, required_fields=None, skip=0):
+    data = []
+    for record in read_json_file_iterator(file_name, required_fields=required_fields, skip=skip):
+        data.append(record)
+    return data
+
+
+class JSONWriter(object):
+    """
+    This class encapsulates a writer of JSON records into a potentially GZIPed file.
+    """
+
+    def __init__(self, file_name):
+        if not file_name:
+            self.output = sys.stdout
+        else:
+            if splitext(file_name)[1] == ".gz":
+                self.output = gzip.open(file_name, "w")
+            else:
+                self.output = open(file_name, "w", encoding="utf-8")
+
+    def __enter__(self):  # this is necessary to be used in "with statement"
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print(f"error processing JSON writer {str(exc_type)}, {str(exc_value)}, {str(traceback)}", file=sys.stderr)
+        return self.close()
+
+    def write(self, json_record, ensure_ascii=False, indent=None):
+        json.dump(json_record, self.output, ensure_ascii=ensure_ascii, indent=indent)
+        self.output.write("\n")
+        self.output.flush()
+
+    def close(self):
+        if self.output is not sys.stdout:
+            self.output.close()
+
+
+def all_values(json_rec, field_name, subfield_name=None):
+    """
+    This is a helper function that, given a field name, returns an iterator over all values in the field regardless of
+    if the field contains an array or a single value.
+    :param json_rec: JSON record
+    :param field_name: field to contain either array or a single value
+    :return: iterator over the values
+    """
+    if field_name not in json_rec:
+        return
+
+    field = json_rec[field_name][subfield_name] if subfield_name else json_rec[field_name]
+
+    if isinstance(field, list):
+        for k in field:
+            yield k
+    else:
+        yield field
+
+
+def traverse_dir_to_json(output_file, directory):
+    """
+    Traverse a directory recursively and, for each file in the tree, add a single JSON line into an output JSON file:
+    { "_file": "path/excluding/the/directory/file.ext" }
+    :param output_file: JSON file to generate (it's overwritten if exists)
+    :param directory: directory to traverse recursively
+    :return: true, if the file was generated alright
+    """
+    output = JSONWriter(output_file)
+    try:
+        for subdir, dirs, files in os.walk(directory):
+            for file_name in files:
+                output.write({Record.FILE: os.path.join(subdir, file_name)})
+    finally:
+        output.close()
---
+++ tools/json_uploader.py
@@ -0,0 +1,77 @@
+import json
+from argparse import ArgumentParser
+
+from ximilar.client import RecognitionClient
+from ximilar.client.constants import DEFAULT_WORKSPACE, NORESIZE, LABELS, TEST_IMAGE, META_DATA
+from ximilar.tools.json_data import read_json_file_iterator
+
+
+class LabelMock:
+    def __init__(self, id):
+        self.id = id
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Image uploader from JSON records into Ximilar App")
+    parser.add_argument("--api_prefix", type=str, help="API prefix", default="https://api.ximilar.com/")
+    parser.add_argument("--auth_token", help="user authorization token to be used for API authentication")
+    parser.add_argument("--workspace_id", help="ID of workspace to upload the images into", default=DEFAULT_WORKSPACE)
+    parser.add_argument("--input_file", help="JSON file with records to be uploaded")
+    parser.add_argument("--no_resize", help="flag whether to preserve image size", action="store_true")
+    parser.add_argument("--test_image", help="mark the image as test", action="store_true")
+    parser.add_argument("--resize", type=int, default=1024, help="size of uploaded images, default: 1024")
+    parser.add_argument("--print_details", help="if true, info about each image is printed out", action="store_true")
+    parser.add_argument("--label_fields", help="list of JSON fields to take label names from", nargs="+")
+    parser.add_argument("--create_labels", help="create labels from the fields, if don't exist", action="store_true")
+    parser.add_argument("--one_image_per_label", help="upload images with more labels more times", action="store_true")
+    parser.add_argument("--dryrun", help="if true, just print info", action="store_true")
+    args = parser.parse_args()
+
+    client = RecognitionClient(
+        token=args.auth_token, endpoint=args.api_prefix, workspace=args.workspace_id, max_image_size=args.resize
+    )
+
+    labels = {}
+    if len(args.label_fields) > 0:  # find the labels by search (don't create them)
+        all_labels, _ = client.get_all_labels()
+        labels = {l.name: l for l in all_labels}
+
+    records = []
+    for rec in read_json_file_iterator(args.input_file):
+        rec_labels = []
+        for f in args.label_fields:
+            if f not in rec or not rec[f] or len(rec[f]) == 0:
+                continue
+            # if labels should be added, try to find them in the labels
+            if rec[f] not in labels and args.create_labels:
+                if args.dryrun:
+                    print("would create label: " + str(rec[f]))
+                else:
+                    labels[rec[f]], _ = client.create_label(rec[f], "automatically created label")
+                    print("created label: " + str(labels[rec[f]]))
+            if rec[f] in labels:
+                rec_labels.append(labels[rec[f]].id)
+
+        rec[META_DATA] = rec.copy()
+        rec[TEST_IMAGE] = args.test_image
+        rec[NORESIZE] = args.no_resize
+        if args.one_image_per_label and len(rec_labels) > 1:
+            for lab in rec_labels:
+                new_rec = rec.copy()
+                new_rec[LABELS] = [lab]
+                records.append(new_rec)
+        else:
+            rec[LABELS] = rec_labels
+            records.append(rec)
+
+    if args.dryrun:
+        print(json.dumps(records, indent=2))
+    else:
+        results = client.parallel_records_processing(records, client.upload_images, max_workers=5, output=True)
+
+        for (images, status) in results:
+            if status["status"] != "OK":
+                print("error status: " + str(status))
+            if args.print_details:
+                for image in images:
+                    print(image.img_path + " (" + str(image.img_width) + "px x " + str(image.img_height) + "px)")
