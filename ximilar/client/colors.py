51efea5bf1a63459cd8694164565e902875364a5
David Novak <novak.david@gmail.com>
2019-11-29 21:02:30 +0100

    fix

--- ximilar/client/colors.py
+++ ximilar/client/colors.py
@@ -1,3 +1,5 @@
+import json
+
 from ximilar.client import RestClient
 from ximilar.client.constants import *
 
@@ -12,32 +14,16 @@ class DominantColorClient(RestClient):
 
 
 class DominantColorGenericClient(DominantColorClient):
-    def __init__(
-        self, token, collection=None, collection_id=None, endpoint=ENDPOINT, resource_name=DOMINANT_COLORS_PRODUCT
-    ):
-        super(DominantColorGenericClient, self).__init__(
-            token=token,
-            collection=collection,
-            collection_id=collection_id,
-            endpoint=endpoint,
-            resource_name=resource_name,
-        )
+    def __init__(self, token, endpoint=ENDPOINT, resource_name=DOMINANT_COLORS_PRODUCT):
+        super(DominantColorGenericClient, self).__init__(token=token, endpoint=endpoint, resource_name=resource_name)
 
     def dominantcolor(self, records):
         return super().dominantcolor(records, DOMINANT_COLOR_GENERIC_ENDPOINT)
 
 
 class DominantColorProductClient(DominantColorClient):
-    def __init__(
-        self, token, collection=None, collection_id=None, endpoint=ENDPOINT, resource_name=DOMINANT_COLORS_GENERIC
-    ):
-        super(DominantColorProductClient, self).__init__(
-            token=token,
-            collection=collection,
-            collection_id=collection_id,
-            endpoint=endpoint,
-            resource_name=resource_name,
-        )
+    def __init__(self, token, endpoint=ENDPOINT, resource_name=DOMINANT_COLORS_GENERIC):
+        super(DominantColorProductClient, self).__init__(token=token, endpoint=endpoint, resource_name=resource_name)
 
     def dominantcolor(self, records):
         return super().dominantcolor(records, DOMINANT_COLOR_PRODUCT_ENDPOINT)
