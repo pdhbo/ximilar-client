25413a9e310d42afa3c31b123b2e3562ae54aaa2
David Novak <novak.david@gmail.com>
2020-06-30 14:09:18 +0200

    making the insert script more generic (adding params)

--- tools/collections/index_json_file.py
+++ tools/collections/index_json_file.py
@@ -1,10 +1,6 @@
-import os
 from argparse import ArgumentParser
 
 from ximilar.client import SimilarityPhotosClient, SimilarityProductsClient
-from ximilar.client.constants import DEFAULT_WORKSPACE
-from ximilar.client.recognition import Image
-from ximilar.client.search import SIMILARITY_PHOTOS, SIMILARITY_PRODUCTS
 from ximilar.client.utils.json_data import read_json_file_list
 
 
@@ -16,14 +12,16 @@ def clean_fields(index_images, fields):
                 del img[field]
     return index_images
 
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Train all non trained tasks of workspace")
-    parser.add_argument("--api_prefix", type=str, help="API prefix", default="")
-    parser.add_argument("--clean_fields", type=str, help="list of field (separated by comma) to remove from records", default="")
+    parser.add_argument("--api_prefix", help="API prefix", default="")
+    parser.add_argument("--clean_fields", help="list of field (separated by comma) to remove from records", default="")
     parser.add_argument("--auth_token", help="user authorization token to be used for API authentication")
-    parser.add_argument("--collection_id", help="ID of collection to upload the images into", default="")
-    parser.add_argument("--path", help="path to the json file", default="")
-    parser.add_argument("--type", help="product or generic", default="generic")
+    parser.add_argument("--collection_id", help="ID of collection to upload the images into", required=True)
+    parser.add_argument("--path", help="path to the json file", required=True)
+    parser.add_argument("--type", help="product or generic or visual", default="generic")
+    parser.add_argument("--is_array", help="true just in case the data is JSON array", default=False, type=bool)
 
     args = parser.parse_args()
 
@@ -32,22 +30,15 @@ if __name__ == "__main__":
         kwargs["endpoint"] = args.api_prefix
 
     if args.type == "generic":
-        client = SimilarityPhotosClient(
-            token=args.auth_token, collection_id=args.collection_id, **kwargs
-        )
+        client = SimilarityPhotosClient(token=args.auth_token, collection_id=args.collection_id, **kwargs)
     elif args.type == "product":
-        client = SimilarityProductsClient(
-            token=args.auth_token, collection_id=args.collection_id, **kwargs
-        )
+        client = SimilarityProductsClient(token=args.auth_token, collection_id=args.collection_id, **kwargs)
     elif args.type == "visual":
-        client = SimilarityProductsClient(
-            token=args.auth_token, collection_id=args.collection_id, **kwargs
-        )
+        client = SimilarityProductsClient(token=args.auth_token, collection_id=args.collection_id, **kwargs)
     else:
         raise Exception("Please specify one of the similarity type (generic, product, visual)")
 
-
-    index_images = read_json_file_list(args.path)
+    index_images = read_json_file_list(args.path, is_array=args.is_array)
     if args.clean_fields:
         index_images = clean_fields(index_images, args.clean_fields)
 
