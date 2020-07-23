96967a6ea494ae5bec39d393cbec34dff88b030a
Libor Vanek <mail@liborvanek.software>
2020-07-23 16:18:50 +0200

    cliend, delete fix

--- ximilar/client/client.py
+++ ximilar/client/client.py
@@ -6,6 +6,7 @@ import os
 import re
 import numpy as npupda
 import concurrent.futures
+import urllib.parse
 
 from tqdm import tqdm
 
@@ -133,8 +134,9 @@ class RestClient(object):
         """
         self.invalidate()
 
+        url = urllib.parse.urljoin(self.endpoint, api_endpoint)
         result = requests.delete(
-            self.endpoint + api_endpoint, params=params, headers=self.headers, data=data, timeout=self.request_timeout
+            url, params=params, headers=self.headers, data=data, timeout=self.request_timeout
         )
 
         if result.status_code == HTTP_NO_CONTENT_204:
