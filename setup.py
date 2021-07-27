34b305fba34fc9bf6832fb97f073842988919ff0
MichalLukac <cospelthetraceur@gmail.com>
2021-07-27 15:17:02 +0200

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ximilar-client",
-    version="1.21.06",
+    version="1.21.07",
     description="The Ximilar App and Vize.ai Client.",
     url="https://gitlab.com/ximilar-public/ximilar-vize-api",
     author="Michal Lukac, David Novak and Ximilar.com Team",
