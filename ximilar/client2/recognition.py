2eb40fbdb3b4eddad2e6a225d3f1ab96100a5d01
Alexey Voinov <alexey.v.voinov@gmail.com>
2021-10-05 20:21:33 +0200

    read recog label

--- ximilar/client2/recognition.py
+++ ximilar/client2/recognition.py
@@ -16,19 +16,20 @@ class RecognitionClient:
         """
         Creates a new label of type tag on the server
         """
-        return self._new_label({"name": name, "type": "tag", "description": description, "output_name": output_name})
+        reply = self._new_label({"name": name, "type": "tag", "description": description, "output_name": output_name})
+        return Label(self._endpoint, json=reply)
 
     def label(self, label_id: str):
         """
         Returns a structure with all the label properties
         """
-        return RecognitionLabel(self._endpoint, label_id=label_id)
+        return Label(self._endpoint, label_id=label_id)
 
     def _new_label(self, args):
         return self._endpoint.post("recognition/v2/label/", args=args)
 
 
-class RecognitionLabel:
+class Label:
     """
     Wrapper for Ximial Recognition application's label object
     """
@@ -36,23 +37,63 @@ class RecognitionLabel:
     def __init__(self, endpoint, label_id: str = None, json: Dict[str, Any] = None):
         self._endpoint = endpoint
         if json is not None:
-            self._from_json(json)
+            self._id = json["id"]
+            self._data = self._extract_from(json)
         elif id is not None:
             self._id = label_id
 
+    def label_id(self):
+        """Returns the id of the label"""
+        return self._id
+
+    def name(self):
+        """Returns the name of the label"""
+        return self._data["name"]
+
+    def type(self):
+        """Returns the type of the label"""
+        return self._data["type"]
+
+    def description(self):
+        """Returns the description of the label"""
+        return self._data["description"]
+
+    def output_name(self):
+        """Returns the output name of the label"""
+        return self._data["output_name"]
+
+    def images_count(self):
+        """Returns the number of images associated with the label"""
+        return self._data["images_count"]
+
+    def tasks_count(self):
+        """Returns the number of tasks associated with the label"""
+        return self._data["tasks_count"]
+
+    def objects_count(self):
+        """Returns the number of objects associated with the label"""
+        return self._data["objects_count"]
+
+    def negative_for_task(self):
+        """Returns unknown shit"""
+        return self._data["negative_for_task"]
+
+    @staticmethod
+    def _extract_from(data):
+        return {
+            "name": data["name"],
+            "type": data["type"],
+            "description": data.get("description", None) or "",
+            "output_name": data.get("output_name", None) or data["name"],
+            "images_count": data.get("images_count", 0),
+            "tasks_count": data.get("tasks_count", 0),
+            "objects_count": data.get("objects_count", 0),
+            "negative_for_task": data.get("negative_for_task", None),
+        }
+
     def __getattr__(self, item):
         if item == "_data":
-            self._data = self._load_data()
+            reply = self._endpoint.get(f"recognition/v2/label/{self._id}/")
+            self._data = self._extract_from(reply)
+            return self._data
         raise AttributeError(self, item)
-
-    def _load_data(self):
-        data = self._endpoint.get(f"/recognition/v2/label/{self._id}/")
-        self._from_json(data)
-
-    def _from_json(self, data):
-        self._data["id"] = data["id"]
-        self._data["name"] = data["name"]
-        self._data["type"] = data["type"]
-        if "description" in data:
-            self._data["description"] = data["description"]
-        self._data["outputname"] = data["output_name"]
