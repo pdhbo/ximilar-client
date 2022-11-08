a9fe63f64ea6c348fde840591d2c5e86b60e280e
MichalLukac <cospelthetraceur@gmail.com>
2022-11-08 10:39:21 +0100

    Adding custom client.

---
+++ ximilar/client/custom.py
@@ -0,0 +1,21 @@
+from ximilar.client.recognition import RecognitionClient
+from ximilar.client.constants import *
+
+
+class CustomPredictClient(RecognitionClient):
+    def __init__(self, token, endpoint=ENDPOINT, resource_name="custom-model"):
+        super().__init__(token=token, endpoint=endpoint, resource_name=resource_name)
+        self.PREDICT_ENDPOINT = "custom/v2/predict"
+
+    def predict(self, records, task_id, version=None):
+        """
+        Takes the images and calls the ximilar client for extracting custom outputs.
+        """
+        # version is default set to None, so ximilar will determine which one to take
+        data = self.construct_data(records=records, task_id=task_id, version=version)
+        print(data)
+        result = self.post(self.PREDICT_ENDPOINT, data=data)
+
+        print(result)
+        self.check_json_status(result)
+        return result
